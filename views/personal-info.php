b9e41d5e98a58456b9b8d3399b818c52e9549e2c
Brandon Skar <bransk01@yahoo.com>
2019-01-16 01:15:35 -0500

    Organized code, modified some other pages. Added code snippets for better readability

--- views/personal-info.php
+++ views/personal-info.php
@@ -14,9 +14,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include('include/styles.html'); ?>
     <title>Personal Information</title>
 </head>
 <body>
+<?php include('include/navbar.html'); ?>
 
+
+
+
+<?php include('include/scripts.html'); ?>
 </body>
 </html>
