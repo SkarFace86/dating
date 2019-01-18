d1a1a82f2af8687cfdb0d3a41749dd48d4c28668
Brandon Skar <bransk01@yahoo.com>
2019-01-18 18:00:06 -0500

    Added submit button and set up terms of service window

--- views/personal-info.php
+++ views/personal-info.php
@@ -32,39 +32,51 @@ $phone = "";
         <hr class="alert-secondary">
     <div class="row">
         <div class="col-7">
-            <label class="font-weight-bold w-100">First Name
+            <form>
+                <label class="font-weight-bold w-100">First Name
+                    <br>
+                    <input type="text" name="fname" class="w-100"
+                        <?php echo "value='$name'"; ?> >
+                </label> <!-- First Name -->
                 <br>
-                <input type="text" name="fname" class="w-100"
-                    <?php echo "value='$name'"; ?> >
-            </label> <!-- First Name -->
-            <br>
-            <label class="font-weight-bold w-100">Last Name
+                <label class="font-weight-bold w-100">Last Name
+                    <br>
+                    <input type="text" name="lname" class="w-100"
+                        <?php echo "value='$lname'"; ?> >
+                </label> <!-- Last Name -->
                 <br>
-                <input type="text" name="lname" class="w-100"
-                    <?php echo "value='$lname'"; ?> >
-            </label> <!-- Last Name -->
-            <br>
-            <label class="font-weight-bold w-100">Age
+                <label class="font-weight-bold w-100">Age
+                    <br>
+                    <input type="text" name="age" class="w-100"
+                        <?php echo "value='$age'"; ?> >
+                </label> <!-- Age -->
                 <br>
-                <input type="text" name="age" class="w-100"
-                    <?php echo "value='$age'"; ?> >
-            </label> <!-- Age -->
-            <br>
-            <label name="gender"><span class="font-weight-bold">Gender</span>
-                <br>
-                <input type="radio" value="male" name="method"
-                        class="font-weight-normal">&nbspMale
-                &nbsp&nbsp
-                <input type="radio" value="female" name="method">&nbspFemale
-            </label> <!-- Gender -->
-            <label class="font-weight-bold w-100">Phone
-                <br>
-                <input type="text" name="phone" class="w-100"
-                         placeholder="(XXX)-XXX-XXXX"
-                       <?php echo "value='$phone'"; ?> >
-            </label>
+                <label name="gender"><span class="font-weight-bold">Gender</span>
+                    <br>
+                    <input type="radio" value="male" name="method"
+                           class="font-weight-normal">&nbspMale
+                    &nbsp&nbsp
+                    <input type="radio" value="female" name="method">&nbspFemale
+                </label> <!-- Gender -->
+                <label class="font-weight-bold w-100">Phone
+                    <br>
+                    <input type="text" name="phone" class="w-100"
+                           placeholder="(XXX)-XXX-XXXX"
+                        <?php echo "value='$phone'"; ?> >
+                </label>
         </div> <!-- col-7 -->
+        <div class="col-5 tos bg-secondary">
+
+        </div>
     </div> <!-- row col-12 -->
+    <div class="row">
+        <div class="col-10"></div> <!-- empty space -->
+        <div class="col-2 align-center">
+            <input type="submit" value="Next >" method="POST" action=""
+                   class="btn btn-primary mx-5">
+        </div>
+    </div> <!-- empty space -->
+            </form> <!-- end form -->
 </div> <!-- div class container -->
 
 
