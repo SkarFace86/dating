d2ca6d50a024ded24500f6b39fda85c16eb7974f
Brandon Skar <bransk01@yahoo.com>
2019-01-28 16:35:32 -0500

    Created validation and reroute to profile page

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ error_reporting(E_ALL);
 
 //Require fat-free
 require_once('vendor/autoload.php');
-
+session_start();
 //Create an instance of the Base class
 $f3 = Base::instance();
 
@@ -27,10 +27,22 @@ $f3->route('GET /', function() {
 });
 
 //Personal information route
-$f3->route('POST /personal-info', function() {
-    $view = new View();
-    echo $view->render('views/personal-info.php');
+$f3->route('POST /personal-info', function($f3) {
+    if(!empty($_POST)) {
+        include('model/personal-info-validation.php');
+        if($validName) {
+            $f3->reroute('profile');
+        }
+    }
+    print_r($_POST);
+
+    $template = new Template();
+    echo $template->render('views/personal-info.html');
 });
 
+$f3->route('GET /profile', function($f3) {
+    $template = new Template();
+    echo $template->render('views/profile.html');
+});
 //Run fat-free
 $f3->run();
--- model/personal-info-validation.php
+++ model/personal-info-validation.php
@@ -1,8 +1,41 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Brandon
+ * User: Brandon Skar
  * Date: 1/22/2019
  * Time: 11:48 PM
  */
 
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$age = $_POST['age'];
+$gender = $_POST['gender'];
+$phone = $_POST['phone'];
+
+$validName = false;
+$validLname = false;
+$validAge = false;
+$validPhone = false;
+
+$phone = str_replace('(', '', $phone);
+$phone = str_replace(')', '', $phone);
+$phone = str_replace('-', '', $phone);
+function validFirstName($name) {
+    return ctype_alpha($name);
+}
+
+function validLastName($name) {
+    return ctype_alpha($name);
+}
+
+function validAge($age) {
+    return ctype_alnum($age) && ($age >= 18);
+}
+
+function validPhone($phone) {
+    return (preg_match("/^[0-9]{10}$/", $phone));
+}
+$f3->set('validFname', (validFirstName($fname)));
+$f3->set('validLname', validLastName($lname));
+$f3->set('validAge', (validAge($age)));
+$f3->set('validPhone', (validPhone($phone)));
