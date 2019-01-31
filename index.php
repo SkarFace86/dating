bbf9172087081f363a7f2fd87dce54277cb6e467
Brandon Skar <bransk01@yahoo.com>
2019-01-31 15:53:54 -0500

    created summary page, updated index routing and validation

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ error_reporting(E_ALL);
 //Require fat-free
 require_once('vendor/autoload.php');
 session_start();
+
 //Create an instance of the Base class
 $f3 = Base::instance();
 
@@ -36,6 +37,14 @@ $f3->route('GET|POST /personal-info', function($f3) {
     $age = $_POST['age'];
     $gender = $_POST['gender'];
     $phone = $_POST['phone'];
+    if($_POST['gender'] == 'male') {
+        $f3->set('male', "checked='checked'");
+        $f3->set('female', "");
+    }
+    else if($_POST['gender'] == 'female') {
+        $f3->set('male', "");
+        $f3->set('female', "checked='checked'");
+    }
     if(!empty($_POST)) {
         if(validFirstName($fname) && validLastName($lname) &&
             validAge($age) && validPhone($phone)) {
@@ -52,23 +61,38 @@ $f3->route('GET|POST /personal-info', function($f3) {
 });
 
 $f3->route('GET|POST /profile', function($f3) {
-    if(!empty($_POST)) {
-        print_r($_POST);
-        $_SESSION['email'] = $_POST['email'];
+    if(!empty($_POST) && validEmail($_POST['email'])) {
+        $_SESSION['email'] = strtolower($_POST['email']);
         $_SESSION['state'] = $_POST['state'];
         $_SESSION['seeking'] = $_POST['seeking'];
         $_SESSION['bio'] = $_POST['bio'];
         $f3->reroute('interests');
     }
     include('include/states.php');
+    if($_POST['seeking'] == 'male') {
+        $f3->set('male', "checked='checked'");
+        $f3->set('female', "");
+    }
+    else if($_POST['seeking'] == 'female') {
+        $f3->set('male', "");
+        $f3->set('female', "checked='checked'");
+    }
     $template = new Template();
     echo $template->render('views/profile.html');
 });
 
 $f3->route('GET|POST /interests', function($f3) {
     include('include/interests.php');
+    if(!empty($_POST)) {
+        $f3->reroute('summary');
+    }
     $template = new Template();
     echo $template->render('views/interests.html');
+});
+
+$f3->route('GET|POST /summary', function($f3) {
+    $template = new Template();
+    echo $template->render('views/summary.html');
 });
 
 //Run fat-free
--- model/data-validation.php
+++ model/data-validation.php
@@ -22,9 +22,17 @@ function validAge($age) {
 }
 
 function validPhone($phone) {
+    //Must be a a numeric string of 10 digits
     return (preg_match("/^[0-9]{10}$/", $phone));
 }
+
+function validEmail($email) {
+    return strpos($email, '@') !== false &&
+        strpos(strtolower($email), '.com') !== false;
+}
+
 $f3->set('validFname', (validFirstName($fname)));
 $f3->set('validLname', validLastName($lname));
 $f3->set('validAge', (validAge($age)));
 $f3->set('validPhone', (validPhone($phone)));
+$f3->set('validEmail', (validEmail($email)));
